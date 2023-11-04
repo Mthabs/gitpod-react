2e70071fef76bc5cdede2b23c77936fba9eb932a
Mthabs <>
2023-11-04 15:03:18 +0000

    add props greeting

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -3,6 +3,9 @@ import React from "react";
 //function FunctionalGreeting() {
 //   return <h1>Hello from React!</h1> 
 //}
-const FunctionalGreetingWithProps = () => <h1>Hello!</h1>;
+const FunctionalGreetingWithProps = (props) => {
+    console.log(props);
+    return <h1>Hello!, {props.greeting}</h1>;
+};
 
 export default FunctionalGreetingWithProps;
